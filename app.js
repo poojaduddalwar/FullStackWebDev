f78740207cb72d494435a3e1a2c49844778bd8e1
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-09-28 18:56:21 +0530

    basics of js

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("hello")
+// console.warn("hello warning")
+// console.error("hello error")
+
+console.log(typeof "hello")
+console.log(typeof 'j')
+// these are String
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 12455887512115232115221115152)
+console.log(typeof -1)
+console.log(typeof -1.2)
+
+//boolean
+console.log(typeof false)
+console.log(typeof true)
+// here t and f of true and false should be small 
+
