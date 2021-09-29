6a4f6eb8f4c2bf4e3406dafa9444d6db1db72a5a
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-09-29 19:47:18 +0530

    array and for loop

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-var Heading = document.getElementById("heading")
-var Para = document.getElementsByClassName("para")
-var tags = document.getElementsByTagName("a")
-var query = document.querySelector('#heading')
-var query1 = document.querySelectorAll('.para')
-var Body = document.querySelector("body")
-// as id is unique so getElementById returns only single thing
-// but class are not unique so getElementsByClassName so returns all the things which uses the class "para"
+var list = document.querySelector('ul')
+console.log(list)
 
-// Heading.innerHTML = "hello pooja"
-// innerHTML is a method provided by the DOM api and is used to replace the content provided with the actual content
 
-console.log(Heading)
-console.log(Para.length)
-console.log(Para[0])
-console.log(Para[1])
-console.log(Para)
-console.log(tags)
-console.log(query)
-console.log(query1)
-
-// Para.push("pooja")
-
-Heading.innerHTML="hello"
-Heading.style = "color:red ; font-size:4rem;"
-
-Heading.classList.add("five")
-Heading.classList.remove("one")
-
-console.log(Heading.classList)
-
-Body.classList.add("dark")
+var arr = ["log","pooja","gym"]
+arr.push("complete")
+for(var i = 0; i< arr.length ; i++){
+    var element = document.createElement("li")
+    var textNode = document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
