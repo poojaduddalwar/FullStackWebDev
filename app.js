3b7b83b4b0462b5e99842f762b1c1b9e93459804
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-01 01:00:31 +0530

    some todo list implementation

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-//Call back function
-//executed after something is completed
-// Calls a defined callback function on each element of an array, and returns an array that contains the results
+var button =document.querySelector('button')
+var input = document.querySelector('input')
+var list = document.querySelector('ul')
 
-var arr = ['go to gym','eat food','clean house']
-
-// for(var i=0; i<arr.length ; i++){
-//     console.log(arr[i])
+// const callBackFunc = (event) => {
+//     console.log(event.target)
+//     // here it shows the targeted html tag 
+//     console.log(input.value)
+//     // here it shows the value we entered in the placeholder inside the console
 // }
 
-// arr.forEach() = this is a array method that allows to iterate over each element of the array.its a call back function
-// var CallBackFunction = (element)=> {
-//     console.log("i ran "+element)
-// // here instead of element we can use any name
-// }
-// arr.forEach(CallBackFunction)
+const callBackFunc = (event) => {
+    const inputValue = input.value
+    // if we want the value of any input use this value attribute
+    // input is a self closing tag and self closing tags don't have any text inside so we use the value attribute for input
+    console.log(input.value)
+    // if we want the text of any element we use the innerHTML attribute
+    console.log(button.innerHTML)
+    const element = document.createElement("li")
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
 
-//shorthand for above
-arr.forEach((element , index) => {
-    console.log(element,index)
-})
-
+button.addEventListener('click',callBackFunc)
