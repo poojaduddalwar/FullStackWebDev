1cf75e4e27770d8279c1d61bad2ef6a09505e71d
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-01 00:10:13 +0530

    arrow functions extra

--- app.js
+++ app.js
@@ -1,44 +1,32 @@
-// Functions in js
-//these are function keyword declaration
-// function greet(Name="pd",age="14"){
-//     console.log(Name,age)
-//     console.log("hello "+Name+" your age is "+age)
-// }
-// // greet("pooja",20)
-// greet()
+// console.log(add(10,20))
 
-// function add(a,b){
-//     // console.log(a+b)
-//     var sum =a+b
+// var add = function(a=0,b=0){
+//     console.log("pd")
+//     var sum = a+b
 //     return sum
-//     // the variable inside this function cannot be accessed anywhere outside the function
 // }
-// var result = add(10,20)
-// console.log(result)
+// // console.log(add(10,20))
+
+// function add(a=0,b=0){
+//     console.log("declaration")
+//     var sum = a+b
+//     return sum
+// }
 // console.log(add(10,20))
 
 
-//function expression
-// assigns a function to a variable
-var add = function (a=0,b=0){
-    var sum =a+b
-    return sum
-}
-console.log(add(10,20))
 
-// var a = function (name, age) {
-//     // console.log('Hello')
-//     console.log(name, age)
-//     // console.log('Hey ' + name + " Your age is " + age)/
-// }
-// console.log(a("pd",20))
+//arrow function shortcut
 
-
-//arrow function
-// arrow function always requires a variable
-// var add = (a=0,b=0)=>{
-//          var sum =a+b
-//          return sum
-// }
-
+// var add = (a=0,b=0) => a+b
 // console.log(add(10,20))
+
+// var add = (a=0,b=0) => {
+//     return a+b
+// }
+// console.log(add(20,20))
+
+// const greet = Name => "hi "+ Name
+// console.log(greet("pooja"))
+
+
