dbe9917afc2574c8e6f062a9bb94814e3ed13e86
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-01 19:25:43 +0530

    array methods

--- app.js
+++ app.js
@@ -1,60 +1,21 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const Add = document.getElementById('add')
-const Multiply = document.querySelectorAll('#multiply')[0]
-const Sub = document.getElementById('subtract')
-// const resultBox = document.querySelector('span')
-const resultBox = document.querySelector('.result')
+var arr = [1,2,3,4,5,6,7,8,9]
+var new_arr = arr.filter((element,index) => {
+    if (element % 2 === 0) return true
+})  //element % 2 === 0 this is just a test
 
-// console.log(a,b)
-// console.log(Add,Multiply)
-// console.log(Result)
+// arr.push(6)
+// //adds element at the end
+// arr.unshift(8)
+// //adds at the begining of the array
+// arr.shift()
+// //removes the first element of the array
+// console.log(arr.unshift())
+// var new_arr = arr.slice(2,5)
+// slice function returs a new array so the original array remains unchanged
 
-// const addFunction = () => {
-//     const Result = parseInt(a.value) + parseInt(b.value)
-//     // console.log(Result)
-//     resultBox.innerHTML = Result
+// for(var i=0;i<arr.length;i++){
+//     if (arr[i] % 2 ===0) new_arr.push(arr[i])
 // }
+//this for loop is used to make array of even nums from arr
 
-// const multiplyFunction = () => {
-//     const MResult = parseInt(a.value)*parseInt(b.value)
-//     resultBox.innerHTML = MResult
-
-// }
-
-const calculator = (event,operation) => {
-    // console.log(poo.target)
-    // if (operation == 'add1'){
-    //     console.log('adding')
-    // }else if (operation == 'multiply1'){
-    //     console.log('multiply')
-    // }
-
-    //switch case
-    switch(operation){
-        case "add1":
-            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
-            break
-        case "multiply1":
-            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
-            break
-            //yoy must give a break after each case
-        case "sub1":
-            resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)
-            break
-        default:
-            resultBox.innerHTML = "not a valid operation"
-    }
-}
-
-// parseInt = converts string to int 
-
-Add.addEventListener('click',(event) => calculator(event,'add1'))
-// here () this dumy function returs another function 
-// this dumy function calls the function calculator
-Multiply.addEventListener('click',(event) => {
-    calculator(event,'multiply1')
-})
-Sub.addEventListener('click',(event)=> calculator(event,'sub1'))
-
-// in the addEventListener the 2nd parameter should be the function signature not a function call 
+console.log(new_arr)
