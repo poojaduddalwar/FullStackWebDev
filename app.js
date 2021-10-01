b8f0f85af985b0f8358b00ab7095c775e67960c4
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-01 19:30:41 +0530

    array splice method

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 var arr = [1,2,3,4,5,6,7,8,9]
-var new_arr = arr.filter((element,index) => {
-    if (element % 2 === 0) return true
-})  //element % 2 === 0 this is just a test
 
-// arr.push(6)
-// //adds element at the end
-// arr.unshift(8)
-// //adds at the begining of the array
-// arr.shift()
-// //removes the first element of the array
-// console.log(arr.unshift())
-// var new_arr = arr.slice(2,5)
-// slice function returs a new array so the original array remains unchanged
+// arr.splice(2,1)
+//Removes elements from an array
+//2 is the index no.
+//1 is the number of elements to be removed
 
-// for(var i=0;i<arr.length;i++){
-//     if (arr[i] % 2 ===0) new_arr.push(arr[i])
-// }
-//this for loop is used to make array of even nums from arr
+console.log(arr.splice(2,4))
 
-console.log(new_arr)
+console.log(arr)
