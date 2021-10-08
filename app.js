08a714fa641f5184b94298c1fc82c608f1b0de49
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-09 02:01:19 +0530

    promises 2 and much more imp  stuff

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
 //promises
-//promise is just an object that promises to return a value .  
-//it may be unknown in the starting but in the future (like 300 milli sec or some milli sec apart) it does return something
+
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+    // console.log(res)
+    let parseData = res.json()
+    //console.log(parseData)  //parseData : it is a promise
+    return (parseData)    //return from the 2nd promise . fetch automaticaly returned a response
+}).then(data => {console.log(data)})
 
 
 
-
+//all promises are thenable
+// .then always returns a promise 
+// promises support then ... when we return a data then what to do?
 
 // setTimeout(() => {
-//     console.log("pooja")
-// }, 0);
-// it is used to create some delay and generaly to simulate  promises they use setTimeout 
-// so here the console.log will run or the call back function will run after the timeout/timer which here is 5000 milli sec which means 5sec. 
+//     const data = response
+//     console.log(data)
+//     console.log(data.json())
+// }, 2000);
 
-// here if the timeout is 0 then also the below console.log statements will execute first and then the settimeout will execute 
+// we use json() method on stream and here we are getting error because the data its a promise 
+// to convert a stream into actual response data we use a json method
 
-// console.log("hi ")
-// console.log("hey")
+// promise is the object . it is created by using the promise constructor
+
+// let value = new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         // reject("pooja")
+//         resolve("hello pooja")
+//     }, 3000);
+// })
+// console.log(value)
+// setTimeout(() => {
+//     console.log(value)
+// }, 4000);
+
+// while creating a new promise we need two parameters resolve and reject . if the promise is fulfilled(resolve) then the resolve function will run (return the value(hello pooja)) and if not the the reject function will run
+
+// promise has four states :- fulfill state , pending state , settled state ,reject state
+// if the promise is fulfilled or rejected it is settled state 
+
+// stack = last in first out (dsa)
+
+//js is single threaded . js has a memory heap that allocates the memory and manages the garbage collection.
+
+// setTimeout is provided by the browser , it is a part of web api. it is a asynchronous task thats why it is executed in the web api .  it is written in c++ that is why it is executed in web api and js cannot run it.
+
+//call stack will run first and then the web api till then from web api the setTimeout after the timer will move to the callback stack and when the call stack is completed the timeout function will run in call stack.
+
+//all the asynchronous tasks like setTimeout that are executed inside the web api they will be sitting inside the callback queue untill the call stack in empty
 
 
+// console.log("one")
+// // setTimeout(() => {
+// //     console.log("timer 1")
+// // }, 5000);
+// setTimeout(() => {
+//     console.log("timer 2")
+// }, 6000);
 
-//fetch //this is the api provided to us by the browser. it fetches the resource for the web.it is used to make a get request to a url
-var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
-console.log(data)
+// setTimeout(() => {
+//     console.log("timer 1")
+// }, 5000);
+// console.log("two")
+// console.log("two")
+// console.log("two")
+// console.log("two")
+// console.log("two")
