788325ad272dfcc369acdfa4dcd81bcc1f24bedd
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-10 00:48:56 +0530

    recipe app 1

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         data.forEach(user => {
-//             console.log(data)
-//         });
-//     })
+var Input = document.querySelector('input')
+var Button = document.querySelector('button')
+// var Div =document.querySelector('div')
 
+// const callBackFun = ()=> {
+//     var inputText = Input.value
+//     Div.innerHTML = inputText
+// }
 
-//async-await
-
-const fetchdata = async () => {
-    // to convert a normal function or synchronous function into asynchronous function use the async keyword.
-
-    //await is only valid or only works in asynchronous function that is why we use async keyword
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    console.log(response)
-    const data = await response.json()
-
-    //same as .then await waits until fetch is resolves
-
-    // here using async-await you can use the await method . here it waits for the promise to resolve(here i.e to fetch the data) and then it stores it in response
-    console.log(data)
+const getData = async (item) => {
+    console.log(item)
+    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data.hits)
 }
 
-fetchdata()
+// Button.addEventListener('click', ()=> {
+//     console.log(Input.value)
+// })
+
+
+Button.addEventListener('click', (e) => { getData(Input.value) })
