a2b262c9b952615b0c80cb778ee93ca72d105bb7
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-09 20:12:40 +0530

    creating new promise

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const list = document.querySelector('ul')
-
-let arr = ['one', 'two', 'three', 'four', 'five']
-
-console.log(list)
-arr.forEach(chores => {
-    const element = document.createElement('li')
-    const textnode = document.createTextNode(chores)
-    element.appendChild(textnode)
-    element.addEventListener('click', (e) => {
-        // console.log(e.target.innerHTML)
-        // e.target.remove()
-        const useInput = window.prompt(`are you sure you want to delete ${chores}`)
-        if (useInput === "YES") e.target.remove()
-    })
-    list.appendChild(element)
-
+const hello = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve('pooja')
+    }, 3000);
 })
+// console.log(hello)
+// setTimeout(() => {
+//     console.log(hello)
+// }, 4000);
+
+hello.then(name => {console.log(name)})
