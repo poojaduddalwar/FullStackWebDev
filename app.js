5b019c30b3bf7063d89b9ce1124986256ab4ca67
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-09 23:36:29 +0530

    accessing the data part 2

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const hello = new Promise((resolve,reject)=>{
-    setTimeout(() => {
-        resolve('pooja')
-    }, 3000);
-})
-// console.log(hello)
-// setTimeout(() => {
-//     console.log(hello)
-// }, 4000);
+// var UL = document.querySelector('ul')
 
-hello.then(name => {console.log(name)})
+// let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
+//     console.log(data)
+//     append_data = (element) => {
+//         const LI = document.createElement("li")
+//         user_details = element['name'] + element['username'] + element['email']
+//         const textnode = document.createTextNode(user_details)
+//         LI.appendChild(textnode)
+//         UL.appendChild(LI)
+//     }
+//     data.forEach(append_data);
+// })
+
+const Table = document.querySelector('table')
+const createRow = (user) => {
+    let newRow = document.createElement('tr')
+    let element = `
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>`
+    newRow.innerHTML = element
+    return (newRow)
+}
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            Table.appendChild(createRow(user))
+        });
+    })
