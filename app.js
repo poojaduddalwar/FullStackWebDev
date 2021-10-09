7a50994a80622332b9a23f9591990b84f8f704f2
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-09 19:54:10 +0530

    delete items from todo list method 2

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var UL = document.querySelector('ul')
+const list = document.querySelector('ul')
 
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => {
-    let parseData = res.json()
-    return (parseData)   
-}).then(data => {
-    console.log(data)
-    data.forEach(element => {
-        const LI = document.createElement("li")
-        const textnode = document.createTextNode(element.name +" " + element.username)
-        LI.appendChild(textnode)
-        UL.appendChild(LI)
-    });
+let arr = ['one', 'two', 'three', 'four', 'five']
+
+console.log(list)
+arr.forEach(chores => {
+    const element = document.createElement('li')
+    const textnode = document.createTextNode(chores)
+    element.appendChild(textnode)
+    element.addEventListener('click', (e) => {
+        // console.log(e.target.innerHTML)
+        // e.target.remove()
+        const useInput = window.prompt(`are you sure you want to delete ${chores}`)
+        if (useInput === "YES") e.target.remove()
+    })
+    list.appendChild(element)
+
 })
