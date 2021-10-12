75c4558ce10c1967d5df8f351bc4f196da55c01d
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-12 17:01:48 +0530

    module - file system

--- app1.js
+++ app1.js
@@ -1,27 +1,35 @@
-const path = require('path') //used to utilize the built in modules
-// console.log(path)  //path module exists to help you with paths in node js 
+//module - filesystem module
 
-//Methods of path module
+const fs = require('fs') //allows to access the disc and used to write on the hard drive .
+// console.log(fs)
+
+//Methods of filesystem module
 
 //1
-console.log(path.resolve('./app1.js'))  //converts relative path to the absolute path
+
+fs.readFile('./text.txt','utf-8',(error,data)=> {
+    if (error) console.log(error)
+    // else console.log(data.toString())
+    else console.log(data)
+})
+//readfile accepts the path and a callback function which has data and error ...so if it fails it throws an error or if it pass it will show the data(buffer data (raw data/compressed data)) buffer is an object
+// if you don't want buffer then add an optional parameter utf8 to the readfile method so that the output is actual text
+// or toString() converts buffer to string 
+
+console.log('i am a random log')
+//line 17 executes first because readFile is asynchronous task(non-blocking I/O task)
+
+console.log(fs.readFileSync('./text.txt','utf8')) //this is a synchronous task 
+//if the file is huge better use asynchronous task but if the file is small use synchronous task
 
 //2
-console.log(path.relative('/','./temp/utils.js'))
-//here it give the path from /(root) to the GIT folder
-// tells the location of GIT folder relative to root 
-//Solve the relative path from {from} to {to}. At times we have two absolute paths, and we need to derive the relative path from one to the other. This is actually the reverse transform of path.resolve.
 
-//3
-console.log(path.extname('./app1.js'))
-//this gives the extension name of the path
+fs.writeFileSync('./text.txt','new stuff inside the file',(error)=> {
+    if (error) console.log(error)
+})
+//this method takes one more parameter that is the text and it replaces/overrite the contents of the text.txt file with the provided text
 
-//4
-console.log(path.dirname('./FullStackWebDev1/utils.js'))
-//this gives the parent directory name where the file is
-
-//5
-console.log(__dirname)
-//this keyword give the directory name of the file we are using (app1.js)
-
-console.log(path.join(__dirname, 'app1.js'))  //joins file with current directory.
+//3 to append 
+fs.writeFile('./text.txt', `Append me ${fs.readFileSync('./text.txt', 'utf8')}`, (error) => {
+    if (error) console.log(error)
+}) 
