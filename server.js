6fbd9e26fe4123137103e6ffba009787cf174b46
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-18 19:50:32 +0530

    difference in requests bet. chrome and local server

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-const data = [{name: "pooja"},{name:'nisha'},{name: "sandhya"},{name:"girish"}]
-//creating server using express
-
 const express = require('express')
-const app = express()
+//require gives only the module.exports of the module which is express
+const PORT = 3000
+const app = express()  //instatiate
 
-//creating route
-app.get('/',(request,response)=> {
-    // console.log(request.url)
-    response.send("this is a response")
+// console.log(express)
+//1. post request from chrome doesn't work- because browser only sends get request
+
+app.get('/',(req,res)=>{
+    console.log(req.headers)
+    res.send('get')
 })
 
-app.get('/getnames',(request,response)=>{
-    response.send(data)
+app.post('/',(req,res) =>{
+    res.send('post')
 })
-app.get('/signup',(request,response)=>{
-    response.send("this is signup")
+
+//2. Different kind of request- with express using thunderclient works well.
+
+app.put('/',(req,res) =>{
+    res.send('put')
 })
-//to make the server listen to requests you have to define routes in express . / route is the homepage route
-app.listen(3000,()=>{
-    console.log("Server listening at port : 3000")
-})   
+
+app.delete('/',(req,res) =>{
+    res.send('delete')
+})
+
+app.patch('/',(req,res) =>{
+    res.send('patch')
+})
+
+
+app.listen(PORT,() => {
+    console.log(`Server running at port :${PORT}`)
+})
+
