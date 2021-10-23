7e4b9e2f1556b06cc51d2d38fb8d1673d85c76ae
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-23 13:10:15 +0530

    dynamic route

--- server.js
+++ server.js
@@ -1,87 +1,20 @@
-// BASIC ECOMMERCE BACKEND
-
 const express = require('express')
 const app = express()
-const PORT = 3000
-const token = "TOP_SECRET"
+const PORT = 3001
 
-app.use(express.json())
+app.get('/products/case')
+app.get('/products/protection')
+app.get('/products/bands')
 
-//Array as Database
-let products = [{ name: "iPhone12 Case", price: "999",id:"1" }, { name: "iPhone13 Case", price: "1999",id:"2" } , { name: "iPhone13 Pro Case", price: "1499",id:"3" }]
-
-//Middlewares
-const validator = (req, res, next) => {
-    const { name, price ,id} = req.body
-    if (!name || !price ||!id) res.json({ error: "validation failed" })
-    else next()
-}
-
-const isAuthorised = (req,res,next)=>{
-    if(req.headers.authorisation === token) next()
-    else res.json({error: "UNAUTHORISED"})
-}
-
-//GET ROUTE - send all the products
-//--------------------Public routes------------------------
-app.get('/products', (req, res) => {
-    //res.send(products) //response is send as a text
-    res.json({ products })
-})
-
-//---------------------Private routes----------------------------
-app.post('/products/add',isAuthorised, validator, (req, res) => {
-    const { name, price ,id} = req.body //destructure
-    // console.log(req.body.price)
-
-    //1 method to add products
-    // const newProduct = {
-    //     name: name,
-    //     price: price,
-    // }
-    // products.push(newProduct)
-
-    //2 method to add products
-    // const newProduct = {
-    //     name,
-    //     price,
-    // }
-    // products.push(newProduct)
-
-    //3 method to add products
-    // products.push({
-    //     name,   //when your key and value pairs variables are same you can pass one variable instead of name: name, price: price
-    //     price,
-    // })
-    // res.send('product added')
-
-    products.push({
-        name,
-        price,
-        id
-    })
-    res.send({ products })
-})
-
-app.delete('/products/delete',isAuthorised,(req,res)=>{
-    const {id} = req.body
-    const newproducts = products.filter(item => item.id != id)
-    res.send({newproducts})
-    products = newproducts
-
-})
-
-////-----------------------------AUTHENTICATION ROUTE FOR ACCESSING PRIVATE ROUTE--------------------------------
-
-app.post('/auth',(req,res)=>{
-    const {email,password} = req.body
-    if(email === 'admin@gmail.com' && password === 'password'){
-        res.send({token})
-    } else{
-        res.send({message: 'UNAUTHORISED'})
+// instead of the abouve 3 seperate routes we can create the below one route 
+app.get('/products/:categories',(req,res)=>{    //dynamic route
+    if(req.params.categories == 'iPhone'){
+        res.send('its an iphone page')
     }
+    console.log(req.params)
+    // res.send('PING')
 })
 
-app.listen(PORT, () => {
-    console.log(`server is up and running at ${PORT}`)
+app.listen(3001,()=>{
+    console.log('Listening to PORT 3001')
 })
