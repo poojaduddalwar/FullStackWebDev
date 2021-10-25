d99dc8d13f338c7350662f1b5bbbd30f9c0325f2
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-25 23:51:31 +0530

    router, uuid package

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const database = require('../database/db')
+const {v4: uuidv4} = require('uuid') //this package creates ids for us.
 
 router.get('/all',(req,res)=>{
     try {
@@ -21,3 +22,32 @@ router.get('/all',(req,res)=>{
         //if we send the above line instead of the res.json part our front end will crash
     }
 })
+
+//to run the routers
+
+router.post('/add',(req,res)=>{
+    try {
+        const {Name} = req.body
+        let newCategory = {
+            Name,
+            id : uuidv4()
+        }
+        database.categories.push(newCategory)
+
+        res.status(200).json({
+            categories: categories,
+            message: 'successfully fetched category1',
+            status: 'SUCCESS1'
+        })
+    } catch (error) {
+        res.status(200).json({
+            categories:[],
+            message:error.message,
+            status:"FAILED1"
+        })
+    }
+})
+
+module.exports = router
+
+//router is an express object that allows us to create routes 
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const database = require('./database/db')
+const categoryRoutes = require('./routes/categoryRoutes')
 const express = require('express')
 const app = express()
 const PORT = 3001
 app.use(express.json())
+
+// console.log(categoryRoutes.stack)
+//http://3001/category/routes
+app.use('/category',categoryRoutes)
 
 app.get("/",(req,res)=>{
     // res.send('hi')
