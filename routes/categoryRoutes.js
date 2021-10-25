481a644d1688b4dd6cf0a05baa22cf9ded0915bb
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-25 19:48:48 +0530

    try catch and status

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,21 +4,20 @@ const database = require('../database/db')
 
 router.get('/all',(req,res)=>{
     try {
-        res.json({
+        res.status(200).json({
             categories: database.categories,
-            message: 'successfull fetched categories',
+            message: 'successfully fetched categories',
             status: 'SUCCESS'
         })
         
     } catch (error) {
-        res.json({
+        res.status(200).json({
             categories:[],
             message:error.message,
             status:"FAILED"
         })
+
+        //res.send(error.message)
+        //if we send the above line instead of the res.json part our front end will crash
     }
 })
-
-
-// whatever you want to execute u write in try block and if it fails the catch block will get executed
-//throw keyword directly throws the error it will not run try catch block
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const database = require('./database/db')
 const express = require('express')
 const app = express()
-// database.products.push('iphone')
-// console.log(database)
 const PORT = 3001
 app.use(express.json())
 
+app.get("/",(req,res)=>{
+    // res.send('hi')
+    try {
+        // document.getElementById('id')
+        console.log(document)
+    } catch (error) {
+        res.status(200).send(error.message)
+    }
+})
 
-app.listen(3001,()=>{
+
+app.listen(PORT,()=>{
     console.log('Listening to PORT 3001')
 })
