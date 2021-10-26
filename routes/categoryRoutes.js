19e5f9d1a4e90e7246e53a0779580eafebfcdd34
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2021-10-27 01:19:26 +0530

    post , delete category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const database = require('../database/db')
-const {v4: uuidv4} = require('uuid') //this package creates ids for us.
+const {v4: uuidv4} = require('uuid') 
+
+
+/*
+ROUTE: http://localhost:3001/category/all
+METHOD : GET
+*/
 
 router.get('/all',(req,res)=>{
     try {
@@ -10,44 +16,75 @@ router.get('/all',(req,res)=>{
             message: 'successfully fetched categories',
             status: 'SUCCESS'
         })
-        
     } catch (error) {
         res.status(200).json({
             categories:[],
             message:error.message,
             status:"FAILED"
         })
-
-        //res.send(error.message)
-        //if we send the above line instead of the res.json part our front end will crash
     }
 })
 
-//to run the routers
+/*
+ROUTE: http://localhost:3001/category/add 
+METHOD : POST
+*/
 
 router.post('/add',(req,res)=>{
-    try {
-        const {Name} = req.body
+    const {Name} = req.body
         let newCategory = {
             Name,
             id : uuidv4()
         }
-        database.categories.push(newCategory)
 
+    try {
+        let include = database.categories.find(item => item.Name === Name)
+        //find expects a callback function and in that function u return the condition if the return condition is true it return the element if it is false it continues.
+        if(!include) database.categories.push(newCategory) 
+        else console.log('Already exixts')
         res.status(200).json({
-            categories: categories,
-            message: 'successfully fetched category1',
-            status: 'SUCCESS1'
-        })
+            categories: database.categories,
+            message: 'successfully added category',
+            status: 'SUCCESS'
+        }) 
     } catch (error) {
         res.status(200).json({
             categories:[],
             message:error.message,
-            status:"FAILED1"
+            status:"FAILED"
+        })
+    }
+})
+
+/*
+ROUTE: http://localhost:3001/category/delete 
+METHOD : DELETE
+*/
+
+router.delete('/delete/:id',(req,res)=>{   //if add the colon(:)the route becomes dynamic so after the colon the id you can add any thing and through the req.params u can get access to it
+    try {
+        // const {id} = req.body
+        const {id} = req.params //here we have to send the id as a query parameter
+        // let element = database.categories.find(item => item.id === id)
+        // const index = database.categories.indexOf(element)
+        // database.categories.splice(index,1)
+
+        const newcategories = database.categories.filter(item => item.id !== id)
+        database.categories = newcategories
+        console.log(newcategories)
+        res.status(200).json({
+            categories: database.categories,
+            message: "Successfully deleted the category",
+            status: "SUCCESS"
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            categories:[],
+            message: error.message,
+            status: "FAILED"
         })
     }
 })
 
 module.exports = router
-
-//router is an express object that allows us to create routes 
--- server.js
+++ server.js
@@ -5,19 +5,15 @@ const app = express()
 const PORT = 3001
 app.use(express.json())
 
-// console.log(categoryRoutes.stack)
-//http://3001/category/routes
 app.use('/category',categoryRoutes)
 
-app.get("/",(req,res)=>{
-    // res.send('hi')
-    try {
-        // document.getElementById('id')
-        console.log(document)
-    } catch (error) {
-        res.status(200).send(error.message)
-    }
-})
+// app.get("/",(req,res)=>{
+//     try {
+//         console.log(document)
+//     } catch (error) {
+//         res.status(200).send(error.message)
+//     }
+// })
 
 
 app.listen(PORT,()=>{
